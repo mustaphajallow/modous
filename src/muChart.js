bd181b668c68428c7a6a2fded168dd64a631956a
root <mustaphajallow369@gmail.com>
2022-07-18 12:15:37 +0000

    new slide

---
+++ src/muChart.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+ 
+import { Line } from 'react-chartjs-2';
+
+import {Chart as ChartJS } from 'chart.js/auto'
+
+
+
+
+
+ function LineChats({chartData}){
+    return(
+        <Line data={chartData} options={{}}/>
+    )
+ }
+
+ 
+  
+
+
+
+ export default LineChats
